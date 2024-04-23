c021c9ab2b8c9b87f3220f3d38704dccec0ddff4
Thomas Moriarty <tojomcmo@gmail.com>
2024-04-23 16:19:08 -0400

    completed animation save and framerate setting for gif output

--- dpend_dyn_animation_main.py
+++ dpend_dyn_animation_main.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import os
 
 from src import dyn_functions as dyn
 from src import visualize_dyn_funcs as vis_dyn
@@ -7,19 +9,28 @@ from src import gen_ctrl_funcs as gen_ctrl
 
 
 if __name__== "__main__":
+    save_ani_bool = True
+
     dt = 0.01
-    len_seq = 2000
-    pend_params = dyn.nlDoublePendParams(g=9.81, m1=1.0, l1=1.0, m2=1.0, l2=1.0, b1=100.0, b2=1.0)
+    len_seq = 500
+    pend_params = dyn.nlDoublePendParams(g=9.81, m1=1.0, l1=1.0, m2=1.0, l2=1.0, b1=0.5, b2=0.2)
     dyn_func = lambda x, u: dyn.double_pend_no_damp_full_act_dyn(pend_params, x, u)
-    x_init = np.array([[1.0],[1.0],[1.0],[1.0]])
-    u_vec = np.array([[0.0],[2.0]]) 
+    x_init = np.array([[1.0],[1.0],[5.0],[-15.0]])
+    u_vec = np.array([[0.0],[0.0]]) 
 
     x_seq = np.zeros((len_seq, 4, 1))
     x_seq[0]= x_init
     for k in range(len_seq-1):
         x_seq[k+1] = gen_ctrl.step_rk4(dyn_func, dt, x_seq[k], u_vec)
 
-    fig = plt.figure(figsize=[5,4])
+    fig = plt.figure(figsize=[10,8])
     pend_animation = vis_dyn.double_pend_animation(pend_params, x_seq, dt,fig)
-    ani = pend_animation.create_double_pend_animation()
-    plt.show()
+    pend_animation.create_double_pend_animation()
+    pend_animation.show_animation()
+
+    if save_ani_bool == True: 
+        print('saving animation...')
+        filename:str | os.PathLike = '/Users/thomasmoriarty/Desktop/dpend_passive'
+        pend_animation.save_animation_gif(filename)
+        print('animation saved!')
+
--- src/visualize_dyn_funcs.py
+++ src/visualize_dyn_funcs.py
@@ -5,6 +5,7 @@ import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 import numpy.typing as npt
+import os
 
 from . import dyn_functions as dyn
 from typing import Tuple
@@ -12,13 +13,12 @@ from typing import Tuple
 
 class double_pend_animation():
     def __init__(self,double_pend_params:dyn.nlDoublePendParams,x_seq:npt.NDArray[np.float64],dt:float, fig:Figure) -> None:
-        self.params = double_pend_params
-        self.dt     = dt
-        self.x_seq  = x_seq        
-        self.fig    = fig
-        self.framerate:int = 30
-
-        self.cartesian_vecs:Tuple = self.create_cartesian_sequence()
+        self.params  = double_pend_params
+        self.dt      = dt
+        self.x_seq   = x_seq        
+        self.fig     = fig
+        self.min_fps:int = 30
+        self.set_fps_for_animation()
         self.configure_figure()
 
     def configure_figure(self):
@@ -28,17 +28,18 @@ class double_pend_animation():
         ax.grid()
         self.line, = ax.plot([], [], 'o-', lw=2)
         self.trace, = ax.plot([], [], '.-', lw=1, ms=2)
-        self.time_template = 'time = %.1fs'
+        self.time_template = 'time = %.2fs'
         self.time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
 
     def create_cartesian_sequence(self)->Tuple[npt.NDArray[np.float64],npt.NDArray[np.float64],npt.NDArray[np.float64],npt.NDArray[np.float64]]:
-        x1 =  self.params.l1*np.sin(self.x_seq[:, 0])
-        y1 = -self.params.l1*np.cos(self.x_seq[:, 0])
-        x2 =  self.params.l2*np.sin(self.x_seq[:, 1]) + x1
-        y2 = -self.params.l2*np.cos(self.x_seq[:, 1]) + y1
+        x1 =  self.params.l1*np.sin(self.x_seq_ani[:, 0])
+        y1 = -self.params.l1*np.cos(self.x_seq_ani[:, 0])
+        x2 =  self.params.l2*np.sin(self.x_seq_ani[:, 1]) + x1
+        y2 = -self.params.l2*np.cos(self.x_seq_ani[:, 1]) + y1
         return (x1, y1, x2, y2)
 
     def animate_double_pend(self, i, line, trace, time_text):
+        self.cartesian_vecs = self.create_cartesian_sequence()
         thisx = [0, (self.cartesian_vecs[0][i]).item(), (self.cartesian_vecs[2][i]).item()]
         thisy = [0, (self.cartesian_vecs[1][i]).item(), (self.cartesian_vecs[3][i]).item()]
 
@@ -49,11 +50,37 @@ class double_pend_animation():
         time_text.set_text(self.time_template % (i*self.dt))
         return line, trace, time_text
     
+    def set_fps_for_animation(self):
+        skipMod:int = 1
+        if 1/(self.dt) / self.min_fps <= 1:
+            pass
+        else:
+            skipMod = int(np.floor(1/(self.dt) / self.min_fps))
+        self.x_seq_ani = self.x_seq[0::skipMod]
+        self.fps:int = int(1 / (self.dt * skipMod))
+
+    def update_fps_for_animation(self, new_min_fps):
+        self.min_fps = new_min_fps
+        self.set_fps_for_animation()
 
     def create_double_pend_animation(self):
-        ani = animation.FuncAnimation(self.fig, partial(self.animate_double_pend, 
+        self.ani = animation.FuncAnimation(self.fig, partial(self.animate_double_pend, 
                                                 line = self.line,
                                                 trace = self.trace,
                                                 time_text = self.time_text), 
-                                                len(self.x_seq), interval=self.dt*1000, blit=True)
-        return ani
+                                                len(self.x_seq_ani), interval=self.fps, blit=True)
+
+    def show_animation(self):
+        # self.fig.show()
+        plt.show()
+
+
+    def save_animation_gif(self, filename: str | os.PathLike):
+        writer = animation.PillowWriter(fps=self.fps,
+                                    metadata=dict(artist='Me'))
+        self.ani.save((filename+'.gif'), writer=writer)
+
+    def save_animation_mp4(self, filename: str | os.PathLike):
+        writer = animation.FFMpegWriter(fps=self.fps,
+                                        metadata=dict(artist='Me'))
+        self.ani.save((filename+'.mp4'), writer=writer)
