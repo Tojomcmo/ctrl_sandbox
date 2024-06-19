49e877be0476b45d63b07c4237e6b228a94a6cb6
Thomas Moriarty <tojomcmo@gmail.com>
2024-06-19 15:04:22 -0400

    updated dockerfile name to Dockerfile for potential github action use, updated python-app.yml for github action to test docker

--- scripts/ilqr_scripts/ilqr_acrobot_rel_dyn_main.py
+++ scripts/ilqr_scripts/ilqr_acrobot_rel_dyn_main.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
 
     jax.config.update("jax_enable_x64", True)
     # ------- Define controller configuration -------#
-    save_ani_bool = False
+    save_ani_bool = True
     sim_with_mj = False
     ani_save_location = "media_output/"
     ani_save_name = "acrobot"
